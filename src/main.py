d09e115ac46cac2b2fd0140f94b2bf7435c27887
idoonut <omkar2@berkeley.edu>
2025-04-27 23:54:30 +0530

    Added changes to gitignore.

--- src/main.py
+++ src/main.py
@@ -9,4 +9,6 @@ def orchestrate_all(video_url):
     
 
 if __name__ == "__main__":
-    orchestrate_all("https://live.vkvideo.ru/tangerin/record/8498f588-c24a-4338-bd15-8b5c9ad7d9bb/video")
+    # orchestrate_all("https://live.vkvideo.ru/tangerin/record/8498f588-c24a-4338-bd15-8b5c9ad7d9bb/video")
+    file_name = orchestrate_all("https://live.vkvideo.ru/tangerin/record/5e0983f7-c4eb-42b6-97bb-8f3c74cac370/records")
+    
